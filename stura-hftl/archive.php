40da2226c9ee06bd379c12d0135f3d66faa2217b
Sven Walter <sven@wltr.eu>
2013-04-11 15:53:48 +0200

    Improved archive.

--- stura-hftl/archive.php
+++ stura-hftl/archive.php
@@ -1,9 +1,28 @@
-<?php get_header(); ?>
+<?php
+
+$i = 0;
+
+get_header(); ?>
 		
 <div id="body">
+	<div class="bg-cat bg-cat-uncategorized">
+		<h1>
+			<?php if ( is_day() ) : ?>
+				Archiv: <?php echo get_the_date(); ?>
+			<?php elseif ( is_month() ) : ?>
+				Archiv: <?php echo get_the_date("F Y"); ?>
+			<?php elseif ( is_year() ) : ?>
+				Archiv: <?php echo get_the_date("Y"); ?>
+			<?php else : ?>
+				Blog Archiv
+			<?php endif; ?>
+		</h1>
+	</div>
+	
+	
 	<div id="news-grid" class="grid-row">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<a class="grid-tile grid-tile-5 <?php stura_print_bg_class($post); ?> <?php echo $post->is_last?"grid-tile-last":''; ?>"
+			<a class="grid-tile grid-tile-5 <?php stura_print_bg_class($post); ?> <?php echo (++$i%5==0)?"grid-tile-last":''; ?>"
 				 href="<?php echo get_permalink($post->ID); ?>">
 				<p>
 					<? echo the_title(); ?>
