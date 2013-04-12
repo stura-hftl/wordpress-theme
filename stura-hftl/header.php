43261aa4eb9507e17c1ae89c95bbb482126257c0
Sven Walter <sven@wltr.eu>
2013-04-12 11:02:19 +0200

    Fixed #17 - remove top navi on start page

--- stura-hftl/header.php
+++ stura-hftl/header.php
@@ -26,20 +26,23 @@
 					<?php echo get_search_form( false ); ?>
 				</div>
 			</div>
-			<div id="header-nav" class="grid-row">
-				<a class="grid-tile grid-tile-4 bg-cat bg-cat-studentenrat" href="<?php echo home_url(); ?>/studentenrat/">
-					<p>Studentenrat</p>						
-				</a>
-				<a class="grid-tile grid-tile-4 bg-cat bg-cat-service" href="<?php echo home_url(); ?>/service/">
-					<p>Service</p>						
-				</a>
-				<a class="grid-tile grid-tile-4 bg-cat bg-cat-club" href="<?php echo home_url(); ?>/club/">
-					<p>HfTL-Club</p>						
-				</a>
-				<a class="grid-tile grid-tile-4 grid-tile-last bg-cat bg-cat-sport" href="<?php echo home_url(); ?>/sport/">
-					<p>Sport</p>						
-				</a>
-			</div>
+			<?php if(!is_front_page()): ?>
+				<div id="header-nav" class="grid-row">
+					<a class="grid-tile grid-tile-4 bg-cat bg-cat-studentenrat" href="<?php echo home_url(); ?>/studentenrat/">
+						<p>Studentenrat</p>						
+					</a>
+					<a class="grid-tile grid-tile-4 bg-cat bg-cat-service" href="<?php echo home_url(); ?>/service/">
+						<p>Service</p>						
+					</a>
+					<a class="grid-tile grid-tile-4 bg-cat bg-cat-club" href="<?php echo home_url(); ?>/club/">
+						<p>HfTL-Club</p>						
+					</a>
+					<a class="grid-tile grid-tile-4 grid-tile-last bg-cat bg-cat-sport" href="<?php echo home_url(); ?>/sport/">
+						<p>Sport</p>						
+					</a>
+				</div>
+			<?php endif; ?>
+			
 			<?php if(is_front_page()): ?>
 				<div id="header-picture" class="frontpage"
 					style="background-image: url(<?php echo get_option("stura-bigpicture-frontpage"); ?>)">
