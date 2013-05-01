a633690ce682f6d3827be1d4468209ef176747ea
Tilmann Bach <tilmann@outlook.com>
2013-05-01 18:49:29 +0300

    added meta to post

--- stura-hftl/_post-content.php
+++ stura-hftl/_post-content.php
@@ -4,3 +4,8 @@
 			<div class="text-tile">
 				<?php the_content(); ?>
 			</div>
+			<div class="post-meta">
+					<?php the_date() ?>,&nbsp;
+					<?php substr(get_the_author_meta( 'first_name', $post->post_author ),0,1); ?>
+					<?php substr(get_the_author_meta( 'last_name', $post->post_author ),0,1); ?>
+			</div>
