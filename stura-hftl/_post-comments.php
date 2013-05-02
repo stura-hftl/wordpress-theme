a5203d7f8d264069c35386f5b12af06377b84fe9
Sven Walter <sven@wltr.eu>
2013-05-02 16:18:21 +0200

    Fix for last commit.

--- stura-hftl/_post-comments.php
+++ stura-hftl/_post-comments.php
@@ -2,19 +2,21 @@
 	$comment_args = array('post_id'=>$post->ID);
 ?>
 
-<div id="comments">
-	<div class="text-tile">
-		<h1>Kommentare</h1>
-		<?php comment_form(); ?>
+<?php if(have_comments()): ?>
+	<div id="comments">
+		<div class="text-tile">
+			<h1>Kommentare</h1>
+			<?php comment_form(); ?>
+		</div>
+		
+		<div class="hbar-style">
+			<?php foreach( get_comments($comment_args) as $comment ): ?>
+				<div class="item">
+					<h4><?php echo $comment->comment_author ?></h4>
+					<p class="meta"><?php echo $comment->comment_date ?></p>
+					<p class="text"><?php echo $comment->comment_content ?></p>
+				</div>
+			<?php endforeach; ?>
+		</div>
 	</div>
-	
-	<div class="hbar-style">
-		<?php foreach( get_comments($comment_args) as $comment ): ?>
-			<div class="item">
-				<h4><?php echo $comment->comment_author ?></h4>
-				<p class="meta"><?php echo $comment->comment_date ?></p>
-				<p class="text"><?php echo $comment->comment_content ?></p>
-			</div>
-		<?php endforeach; ?>
-	</div>
-</div>
+<?php endif; ?>
