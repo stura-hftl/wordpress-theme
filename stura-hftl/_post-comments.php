c84b1060f552b67dc2c9a610541c3d518685a930
Sven Walter <sven@wltr.eu>
2013-05-04 14:54:57 +0200

    Fixed #47 - date formatting in comments is wrong

--- stura-hftl/_post-comments.php
+++ stura-hftl/_post-comments.php
@@ -13,7 +13,7 @@
 			<?php foreach( get_comments($comment_args) as $comment ): ?>
 				<div class="item">
 					<h4><?php echo $comment->comment_author ?></h4>
-					<p class="meta"><?php echo $comment->comment_date ?></p>
+					<p class="meta"><?php echo strftime("%d.%m.%Y %H:%M", strtotime($comment->comment_date)) ?></p>
 					<p class="text"><?php echo $comment->comment_content ?></p>
 				</div>
 			<?php endforeach; ?>
